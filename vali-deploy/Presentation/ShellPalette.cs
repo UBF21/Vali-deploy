1290442e6fb1a16dd6b9e4cd2ac5eae399f68d27
Felipe Rafael Montenegro Morriberon <flashgames104@gmail.com>
2026-07-10 02:16:17 -0500

    feat(presentation): agregar ShellPalette (paleta Forest centralizada)

---
+++ vali-deploy/Presentation/ShellPalette.cs
@@ -0,0 +1,16 @@
+using Spectre.Console;
+
+namespace vali_deploy.Presentation;
+
+public static class ShellPalette
+{
+    public static readonly Color Brand = Color.SeaGreen1;
+    public static readonly Color Muted = Color.Grey62;
+    public static readonly Color Warning = Color.DarkOrange3;
+    public static readonly Color Error = Color.IndianRed1;
+
+    public const string BrandTag = "seagreen1";
+    public const string MutedTag = "grey62";
+    public const string WarningTag = "darkorange3";
+    public const string ErrorTag = "indianred1";
+}
